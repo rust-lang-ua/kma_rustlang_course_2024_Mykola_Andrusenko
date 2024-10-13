837922d241ea9d4422dc2cba3e11f0f39ec3974b
AMykolaD <andrusenkomykola@gmail.com>
2024-10-13 23:00:05 +0200

    iters done

--- rustlings/exercises/standard_library_types/iterators4.rs
+++ rustlings/exercises/standard_library_types/iterators4.rs
@@ -1,6 +1,6 @@
 // iterators4.rs
 
-// I AM NOT DONE
+// I AM DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -12,6 +12,10 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    if(num > 0)
+    {
+        num*factorial(num-1)
+    } else { 1 }
 }
 
 #[cfg(test)]
